b98e1bde7512b6e5c1a09f09e9514e4840e73fa5
Brianna Huynh <huynhbri@usc.edu>
2025-10-22 20:19:55 +0000

    completed!

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,6 +1,6 @@
 #ifndef RECCHECK
 //if you want to add any #includes like <iostream> you must do them here (before the next endif)
-
+#include <algorithm>
 #endif
 
 #include "equal-paths.h"
@@ -8,11 +8,42 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
+int getHeight(Node* parent){
+  if(parent == nullptr){
+    return 0;
+  }
+
+  int leftHeight = getHeight(parent->left);
+  int rightHeight = getHeight(parent->right);
+
+  return max(leftHeight, rightHeight) + 1;
+}
 
 
 bool equalPaths(Node * root)
 {
-    // Add your code below
+  // base cases
+  if(root == nullptr){
+    return true;
+  }
+  if(root->left == nullptr && root->right != nullptr){
+    if(root->right->right == nullptr){
+      return true;
+    }
+  }
+  if(root->right == nullptr && root->left != nullptr){
+    if(root->left->left == nullptr){
+      return true;
+    }
+  }
 
+  // call helper function which is recursive
+  int leftHeight = getHeight(root->left);
+  int rightHeight = getHeight(root->right);
+
+  if(leftHeight == rightHeight){
+    return true;
+  }
+  return false;
 }
 
